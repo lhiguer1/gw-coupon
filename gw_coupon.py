5356dea5b5cada6ab4b05dfce66bd0c36c9223d4
Leonel Higuera <lhiguer1@asu.edu>
2021-03-29 18:42:03 -0700

    Refactor "Phase 2" to use dotenv

--- gw_coupon.py
+++ gw_coupon.py
@@ -1,41 +1,37 @@
 from selenium import webdriver
 from random import random
 from time import sleep
+from dotenv import dotenv_values
+from tempMail2 import TempMail
 import os
 
 
 def send_keys(driver, element, msg):
-    '''Send msg to element. sleep() and random() are used to avoid getting blocked from the site'''
+    '''Send msg to element. sleep() and random() are used to look more human'''
     sleep(random())
     for char in msg:
         sleep(random())
         element.send_keys(char)
 
-# temporarily modify path
-os.environ['path'] += os.pathsep + os.path.abspath('webdrivers')
-
-driver = webdriver.Firefox()
-
-driver.get('https://www.goodwillaz.org/coupons-store-specials/')
+dotenv_config = dotenv_values()
 
 # Phase 1: Get temp-mail email
 # Phase 2: Sign up to newsletter
 email = "johnsmith@email.com"
-first_name = "John"
-last_name = "Smith"
-zip_code = "1234"
+driver = webdriver.Firefox(executable_path=os.path.relpath('webdrivers\geckodriver.exe'))
+driver.get(dotenv_config.get('URL'))
 
 element = driver.find_element_by_id('mce-EMAIL')
 send_keys(driver, element, email)
 
 element = driver.find_element_by_id('mce-FNAME')
-send_keys(driver, element, first_name)
+send_keys(driver, element, dotenv_config.get('FNAME'))
 
 element = driver.find_element_by_id('mce-LNAME')
-send_keys(driver, element, last_name)
+send_keys(driver, element, dotenv_config.get('LNAME'))
 
 element = driver.find_element_by_id('mce-MMERGE3')
-send_keys(driver, element, zip_code)
+send_keys(driver, element, dotenv_config.get('ZIP_CODE'))
 
 element = driver.find_element_by_id('mc-embedded-subscribe')
 element.click()
